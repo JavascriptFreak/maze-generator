df612a7b19532d48d771b4f59ec6d5498bae4ed7
Frantisek Musil <frantisek.musil@educanet.cz>
2016-03-17 18:12:40 +0100

    Name conflict removed

--- src/main/scala/generator/Maze.scala
+++ src/main/scala/generator/Maze.scala
@@ -15,9 +15,7 @@ class Maze( size: Int ) extends TMaze {
 
   private def isPossible( x: Int, y: Int ): Boolean = isOnBoard(x, y) && !isFilled(x, y)
 
-  private def getAllDirections(x: Int, y: Int): List[(Int, Int)] = {
-    List((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
-  }
+  private def getAllDirections(x: Int, y: Int): List[(Int, Int)] = List((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
 
   def generate( x: Int = 0, y: Int = 0 ): CellDef = {
 
@@ -29,7 +27,7 @@ class Maze( size: Int ) extends TMaze {
 
       val t: List[(Int, Int)] = Random.shuffle(getAllDirections(x, y))
 
-      CellSome(x, y, t map { case (x,y) => generate(x, y) })
+      CellSome(x, y, t map { case (a,b) => generate(a, b) })
 
     }
 
